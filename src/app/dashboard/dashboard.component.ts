cbedc0e916b15b31865be64411a898e4a1ac24b4
vijaysidda <47773989+vijaysidda@users.noreply.github.com>
2024-02-19 11:34:53 +0530

    Update dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,35 +9,7 @@ import { ApiService } from '../services/api.service';
 })
 export class DashboardComponent implements OnInit {
 
-  productDetails:any = [];
-  emailValue:string | null;
 
-  query = {
-    _start:0,
-    _limit:6
-  }
-
-  is_api_loading:boolean = false;
-  is_reached_max:boolean = false;
-
-  constructor(private api:ApiService,private router:ActivatedRoute,
-    private route:Router) {
-    this.emailValue = this.router.snapshot.paramMap.get('id')
-  }
-
-  ngOnInit() {
-    this.getProducts()
-  }
-
-  getProducts() {
-    this.is_api_loading = true;
-    this.api.getData(this.query).subscribe(data => {
-      this.is_api_loading = false;
-      if(!data.length) this.is_reached_max = true;
-      this.productDetails = [...this.productDetails,...data];
-      this.query._start += 12;
-    })
-  }
 
 
 
